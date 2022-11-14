bc6a6b454b6a2ebd28031520b3983347e663e30c
dekkumo <houper8@gmail.com>
2022-11-14 18:51:21 +0300

    deleted todo

--- script/index.js
+++ script/index.js
@@ -12,10 +12,33 @@ function addTask(event) {
 
   const taskText = taskInput.value; // текс задачи из поля ввода
 
-  const taskHtml = `<li class="block__text">${taskText}</li>`
+  const taskHtml = `<li class="block__text">
+                      <div class="text__wrapper">
+                        <span>${taskText}</span>
+                      </div>
+                      <div class="button__wrapper">
+                        <button class="btn__icon complete">
+                          <img src="icons/yes.png" alt="pic">
+                        </button>
+                        <button class="btn__icon close">
+                          <img src="icons/no.png" alt="pic">
+                        </button>
+                      </div>
+                      </li>`
 
   taskList.insertAdjacentHTML('beforeend', taskHtml); // добавляем задачу на страницу
 
   taskInput.value = '' // очистка поля ввода и возвращение на него фокуса
   taskInput.focus();
+}
+
+
+
+const close = document.querySelector('.close');
+
+taskList.addEventListener('click', closeTask);
+
+function closeTask(event) {
+  let element = event.target.closest('.block__text'); 
+  element.remove()
 }
