19f642637ab07aa533abb5d59ab728d94f731951
dekkumo <houper8@gmail.com>
2022-11-14 11:16:08 +0300

    fix input

--- script/index.js
+++ script/index.js
@@ -5,7 +5,9 @@ const taskInput = document.querySelector('#taskInput');
 const taskList = document.querySelector('#taskList');
 
 
-myForm.addEventListener('submit', function(event) {
+myForm.addEventListener('submit', addTask) 
+
+function addTask(event) {
   event.preventDefault(); // отменяет стандартное поведение
 
   const taskText = taskInput.value; // текс задачи из поля ввода
@@ -13,5 +15,7 @@ myForm.addEventListener('submit', function(event) {
   const taskHtml = `<li class="block__text">${taskText}</li>`
 
   taskList.insertAdjacentHTML('beforeend', taskHtml); // добавляем задачу на страницу
-})
 
+  taskInput.value = '' // очистка поля ввода и возвращение на него фокуса
+  taskInput.focus();
+}
